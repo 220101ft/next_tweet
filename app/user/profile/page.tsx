1f43abf01e9dc59c01270924d336fe9bca29e776
220101ft <220101ft@yse-c.net>
2024-01-17 11:16:16 +0900

    update

--- app/user/profile/page.tsx
+++ app/user/profile/page.tsx
@@ -1,7 +1,11 @@
 "use client";
 
 const ProfilePage = () => {
-    return <div>Enter</div>;
+    return (
+        <div>
+            <h1 className="text-2xl text-center font-bold">Profile</h1>
+        </div>
+    );
 };
 
 export default ProfilePage;
