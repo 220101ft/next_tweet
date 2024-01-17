2d93549eafc31db20b648f792ae9893ca9529ed9
220101ft <220101ft@yse-c.net>
2024-01-17 10:31:21 +0900

    add

---
+++ app/components/NavbarLink.tsx
@@ -0,0 +1,11 @@
+import Link from "next/link";
+
+const NavbarLink = () => {
+    return (
+        <Link href="/user/profile" className="hidden md:inline-block p-3 text-black">
+            Profile
+        </Link>
+    );
+};
+
+export default NavbarLink;
