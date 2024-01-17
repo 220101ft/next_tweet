c791f8023c0bd18b2122e29a279ddb968919b343
220101ft <220101ft@yse-c.net>
2024-01-17 10:04:47 +0900

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,25 +1,19 @@
-import type { Metadata } from 'next'
-import './globals.css'
-import Navbar from '@/app/component/Navbar'
+import Navbar from "@/app/components/Navbar";
+import type { Metadata } from "next";
+import "./globals.css";
 
 export const metadata: Metadata = {
-  title: 'Next Tweet',
-  description: 'This is tweet app.',
-}
+    title: "Next Tweet",
+    description: "This is tweet app.",
+};
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode
-}) {
-  return (
-    <html lang="en">
-      <body>
-        <Navbar />
-        <main className="flex min-h-screen flex-col p-2">
-          {children}
-        </main>
-      </body>
-    </html>
-  )
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+    return (
+        <html lang="ja">
+            <body>
+                <Navbar />
+                <main className="flex min-h-screen flex-col p-5">{children}</main>
+            </body>
+        </html>
+    );
 }
