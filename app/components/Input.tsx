2779b4015aafb233a69a313aed683651c151b62f
220101ft <220101ft@yse-c.net>
2024-01-17 11:54:10 +0900

    update

--- app/components/Input.tsx
+++ app/components/Input.tsx
@@ -1,7 +1,22 @@
-"use client";
+"use client"
+
+const className = `
+            p-3
+            my-2
+            border-2
+            border-gray-200
+            rounded
+            w-full
+            focus:outline-none
+            focus:bg-white
+            focus:border-blue-500
+`
 
 const Input = () => {
-    return <input type="text" className="border" />;
-};
+
+    return (
+        <input type="text" className={className} />
+    );
+}
 
 export default Input;
