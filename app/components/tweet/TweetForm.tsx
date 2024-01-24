255c14b4ce7ae3143cddfdcb6f4cf2459b4f4f8d
220101ft <220101ft@yse-c.net>
2024-01-24 12:11:13 +0900

    update

--- app/components/tweet/TweetForm.tsx
+++ app/components/tweet/TweetForm.tsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import ClickButton from "../ClickButton";
 
 const TweetForm = () => {
     const [message, setMessage] = useState<string>("");
@@ -7,19 +8,15 @@ const TweetForm = () => {
         setMessage(event.target.value);
     };
 
+    const isDisable = () => !message;
+
     return (
         <div>
             <textarea value={message} onChange={messageHandler} className="resize-none w-full h-24 border rounded-md p-2" placeholder="今なにしてる？"></textarea>
+
             <div className="p-3">{message.length} characters.</div>
-            <button
-                className="w-full bg-blue-500 hover:bg-blue-700
-                text-white font-bold 
-                py-3 px-4 mb-2
-                rounded"
-                onClick={() => {}}
-            >
-                Send
-            </button>
+
+            <ClickButton label="Post" onClick={() => {}} disabled={isDisable()} />
         </div>
     );
 };
