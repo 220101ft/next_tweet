dfcf7ebd99978c0d0d45a859ff7dc481eb969478
220101ft <220101ft@yse-c.net>
2024-01-24 10:35:00 +0900

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,18 @@
-"use client"
+"use client";
+
+import { useState } from "react";
+import { User, testUser } from "./models/User";
+import { Tweet } from "./models/Tweet";
 
 export default function Home() {
-  console.log('Home')
-  return (
-    <div>
-      <textarea className="resize-none w-full h-24 border rounded-md p-2"></textarea>
-      <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg">Send</button>
-    </div>
-  )
+    //テストユーザの取得
+    const [user] = useState<User>(testUser);
+    const [tweets, setTweets] = useState<Tweet[]>([]);
+
+    return (
+        <div>
+            <textarea className="resize-none w-full h-24 border rounded-md p-2"></textarea>
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg">Send</button>
+        </div>
+    );
 }
