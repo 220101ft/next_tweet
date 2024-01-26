3684cc3aaa7c71e3baef449957dda52f85645ced
220101ft <220101ft@yse-c.net>
2024-01-26 09:41:03 +0900

    update

--- app/components/tweet/TweetForm.tsx
+++ app/components/tweet/TweetForm.tsx
@@ -10,13 +10,16 @@ const TweetForm = () => {
 
     const isDisable = () => !message;
 
+    // Tweet投稿のイベントハンドラ
+    const onPost = () => {};
+
     return (
         <div>
             <textarea value={message} onChange={messageHandler} className="resize-none w-full h-24 border rounded-md p-2" placeholder="今なにしてる？"></textarea>
 
             <div className="p-3">{message.length} characters.</div>
 
-            <ClickButton label="Post" onClick={() => {}} disabled={isDisable()} />
+            <ClickButton label="Post" onClick={onPost} disabled={isDisable()} />
         </div>
     );
 };
